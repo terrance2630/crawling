d0a4f05d5b82a7613b88f3fd5a4895a5ce5fd81f
terrance2630 <tww2@student.unimelb.edu.au>
2023-06-28 10:28:37 +0800

    author not found bug fixed

--- autohome_handler.py
+++ autohome_handler.py
@@ -18,17 +18,30 @@ def scrape_autohome_page_info(url):
         view_span = soup.select_one('.post-handle-view strong')
         reply_span = soup.select_one('.post-handle-reply strong')
         praise_span = soup.select_one('.post-assist-praise strong')
-        author_link = soup.select_one('.user-name a')
+        # 找到所有的<script>标签
+        script_tags = soup.find_all('script')
 
-        
+        # 遍历所有的<script>标签
+        for script_tag in script_tags:
+            script_content = script_tag.string
+            if script_content is not None and '__TOPICINFO__' in script_content:
+                # 使用字符串操作提取topicMemberId和topicMemberName的值
+                topic_member_id_start = script_content.find("topicMemberId: ") + len("topicMemberId: ")
+                topic_member_id_end = script_content.find(",", topic_member_id_start)
+                topic_member_id = script_content[topic_member_id_start:topic_member_id_end].strip()
+
+                topic_member_name_start = script_content.find("topicMemberName: '") + len("topicMemberName: '")
+                topic_member_name_end = script_content.find("'", topic_member_name_start)
+                topic_member_name = script_content[topic_member_name_start:topic_member_name_end]
+
 
         view_number = view_span.text if view_span else '0'
         reply_number = reply_span.text if reply_span else '0'
         praise_number = praise_span.text if praise_span and praise_span.text else '0'
         with open(f'{view_number}.html', 'w', encoding='utf-8') as f:
             f.write(page_source)
-        author_homepage = author_link['href'] if author_link else "未找到作者主页链接"
-        author_title = author_link['title'] if author_link else "未找到作者标题"
+        author_id = topic_member_id
+        author_title = topic_member_name
 
         driver.quit()
 
@@ -37,7 +50,7 @@ def scrape_autohome_page_info(url):
             "浏览量": view_number,
             "回复量": reply_number,
             "点赞量": praise_number,
-            "作者主页": author_homepage,
+            "作者id": author_id,
             "作者": author_title
         }
 
--- test2.py
+++ test2.py
@@ -1,9 +1,27 @@
 from bs4 import BeautifulSoup
+import json
 
-with open("./qczj-selenium-data.html") as file:
+with open("./17.html") as file:
     html=file.read()
 soup = BeautifulSoup(html,'html.parser')
 
-praise_element = soup.select_one('.post-assist-praise strong')
-praise_number = praise_element.text if praise_element else "Praise number not found."
-print(praise_number)
+# 找到所有的<script>标签
+script_tags = soup.find_all('script')
+
+# 遍历所有的<script>标签
+for script_tag in script_tags:
+    script_content = script_tag.string
+    if script_content is not None and '__TOPICINFO__' in script_content:
+        # 使用字符串操作提取topicMemberId和topicMemberName的值
+        topic_member_id_start = script_content.find("topicMemberId: ") + len("topicMemberId: ")
+        topic_member_id_end = script_content.find(",", topic_member_id_start)
+        topic_member_id = script_content[topic_member_id_start:topic_member_id_end].strip()
+
+        topic_member_name_start = script_content.find("topicMemberName: '") + len("topicMemberName: '")
+        topic_member_name_end = script_content.find("'", topic_member_name_start)
+        topic_member_name = script_content[topic_member_name_start:topic_member_name_end]
+
+        # 打印结果
+        print("topicMemberId:", topic_member_id)
+        print("topicMemberName:", topic_member_name)
+        break
